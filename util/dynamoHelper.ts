817cd7b46b052604025eb512437460d33ffa2863
Jaonl <jason.lu@cloudpbx.ca>
2022-04-24 00:35:04 -0700

    add fake creds to dynamodb

--- util/dynamoHelper.ts
+++ util/dynamoHelper.ts
@@ -1,3 +1,3 @@
 import { DynamoDB } from 'aws-sdk'
 
-export const dynamoDb = new DynamoDB.DocumentClient({ region: 'us-east-1', endpoint: 'http://localhost:8000' })
+export const dynamoDb = new DynamoDB.DocumentClient({ region: 'us-east-1', endpoint: 'http://localhost:8000', accessKeyId: 'xxxxx', secretAccessKey: 'xxxxx' })
