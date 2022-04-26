99925a8d4005a0da447804356c5513d10cfc0f71
Eric Ho <endonoho@gmail.com>
2022-04-26 14:04:43 -0700

    Add language on initial load

--- frontend/src/classes/Language.js
+++ frontend/src/classes/Language.js
@@ -4,11 +4,13 @@ export default class Language {
     constructor(langDbModel) {
         if (typeof langDbModel !== 'undefined') {
             // Construct new Language Object with langDbModel
+						this.id = langDbModel.id;
             this.name = langDbModel.name;
             this.description = langDbModel.description;
             this.link = langDbModel.link;
         } else {
             // Construct new Language Object using default values
+						this.id = '';
             this.name = '';
             this.description = '';
 						this.link = '';
