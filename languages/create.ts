140f77f63417dfdc4f93b6bbd5b24de6f1e8c2b2
Eric Ho <endonoho@gmail.com>
2022-04-26 13:58:14 -0700

    Update handler name, create language handler

--- languages/create.ts
+++ languages/create.ts
@@ -7,23 +7,25 @@ export const create: Handler = async (event: APIGatewayEvent, context: Context) 
   const timestamp = new Date().getTime()
   if (!event.body) return errorResponse(400, `Body is required`)
   const data = JSON.parse(event.body)
-  if (typeof data.text !== 'string') {
+	console.log('data', data);
+  if (typeof data.name !== 'string') {
     console.error('Validation Failed')
-    return errorResponse(400, `Field 'text' is required`)
+    return errorResponse(400, `Field 'name' is required`)
   }
 
   const params = {
     TableName: process.env.DYNAMODB_TABLE as string,
     Item: {
       id: uuid.v1(),
-      text: data.text,
-      checked: false,
+      name: data.name,
+			description: data.description,
+			link: data.link,
       createdAt: timestamp,
       updatedAt: timestamp
     }
   }
 
-  // write the todo to the database
+  // write the language to the database
   try {
     const res = dynamoDb.put(params).promise()
     return successResponse(params.Item)
